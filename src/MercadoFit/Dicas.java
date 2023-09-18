f5ac3644a5facb659af2d671d411ec3527c8587a
marlon <marlon.oliveira34@hotmail.com>
2023-09-18 14:23:09 -0300

    quinto commit

--- src/MercadoFit/Dicas.java
+++ src/MercadoFit/Dicas.java
@@ -11,10 +11,10 @@ public class Dicas extends Informativos{
         dicas2.texto = ("\nB. Treino de Força Explosiva: Incorpore exercícios que requerem movimentos rápidos e explosivos, como os saltos ou levantamentos olímpicos.");
 
                 Dicas dicas3 = new Dicas();
-        dicas3.texto = ("\nC.Treino de Alta Intensidade (HIIT): Integre sessões de HIIT para promover a queima de gordura e melhorar a resistência cardiovascular.");
+        dicas3.texto = ("\nC. Treino de Alta Intensidade (HIIT): Integre sessões de HIIT para promover a queima de gordura e melhorar a resistência cardiovascular.");
 
                 Dicas dicas4 = new Dicas();
-        dicas4.texto = ("\nD.Foco na Mobilidade e Flexibilidade: Dedique tempo para exercícios de mobilidade e alongamento para melhorar a amplitude de movimento e prevenir lesões.");
+        dicas4.texto = ("\nD. Foco na Mobilidade e Flexibilidade: Dedique tempo para exercícios de mobilidade e alongamento para melhorar a amplitude de movimento e prevenir lesões.");
 
         return List.of(dicas1,dicas2,dicas3,dicas4);
     }
--- src/MercadoFit/Menu.java
+++ src/MercadoFit/Menu.java
@@ -1,34 +1,38 @@
 package MercadoFit;
 
-import java.util.InputMismatchException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
-public class Mercado {
+public class Menu {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        int opcao;
+        int opcao,escolha;
         String categoria="0",pessoa;
+        List<String> alimentacao = Arrays.asList("Coma Banana", "Coma Aveia", "Coma Proteinas em geral", "Coma Grãos", "Evitar embutidos", "Evitar Margarina", "Evitar Açucares em excesso");
 
         while (true) {
-            System.out.println("********* Universo Fit Online *********");
+            System.out.println("\n********* Universo Fit Online *********");
             System.out.println(" ");
             System.out.println("      1 - Curiosidades Fitness");
             System.out.println("      2 - Dicas de treino");
-            System.out.println("      3 - Cancelar operação");
+            System.out.println("      3 - Alimentação");
+            System.out.println("      4 - Cancelar operação");
             System.out.println("***************************************");
             System.out.println(" ");
             System.out.println("Digite uma opção para prosseguir:");
 
             opcao = ler.nextInt();
 
-            if (opcao == 3) {
+            if (opcao == 4) {
                     System.out.println("\nVocê saiu do Mercado Fit.");
                     ler.close();
                     System.exit(0);
             }
 
             try {
-                if (opcao == 0 || opcao > 3) {
+                if (opcao == 0 || opcao > 4) {
                     throw new RuntimeException("Digite uma opção válida!");
                 }
             } catch (Exception e) {
@@ -42,13 +46,11 @@ public class Mercado {
                     System.out.println("Algumas Curiosidades Fitness:\n");
                     System.out.println("A. Estudos mostram que ouvir música durante o exercício pode aumentar o desempenho e a resistência, pois pode distrair da fadiga e melhorar o humor.\n");
                     System.out.println("B. O músculo mais forte do corpo humano é o glúteo máximo, que está localizado na parte traseira da coxa.\n");
-                    System.out.println("C. A endorfina, conhecida como o \"hormônio da felicidade\", é liberada durante o exercício e pode gerar uma sensação de bem-estar e reduzir a percepção da dor.\n");
+                    System.out.println("C. A endorfina, conhecida como o hormônio da felicidade, é liberada durante o exercício e pode gerar uma sensação de bem-estar e reduzir a percepção da dor.\n");
                     System.out.println("D. O exercício regular pode melhorar a função cognitiva, a memória e reduzir o risco de distúrbios neurológicos como o Alzheimer.\n");
                     break;
-                case 2:
-                    System.out.println("Qual seu nome?");
-                    pessoa = ler.next();
 
+                case 2:
                     System.out.println("Você é Iniciante ou Avançado na musculação?");
                     categoria = ler.next();
 
@@ -62,6 +64,30 @@ public class Mercado {
                                 System.out.println(lista.texto);
                             }
                     }
+                    break;
+                case 3:
+
+                    System.out.println("Aqui teremos resultados conforme a sua escolha (Digite 1 ou 2)");
+                    System.out.println("1. Alimentos bons\n2. Alimentos a serem evitados");
+                    escolha = ler.nextInt();
+
+                    if (escolha == 1) {
+                        List <String> alimentacaoC = alimentacao.stream()
+                                .filter(x-> x.toString().startsWith("C"))
+                                .collect(Collectors.toList());
+                        for (String i : alimentacaoC) {
+                            System.out.println(i);
+                        }
+
+                    } else if (escolha == 2) {
+                        List <String> alimentacaoE = alimentacao.stream()
+                                .filter(x-> x.toString().startsWith("E"))
+                                .collect(Collectors.toList());
+                        for (String i : alimentacaoE) {
+                            System.out.println(i);
+                        }
+                    }
+                    break;
             }
         }
     }
